62d7e25f8981bdc775c03f6ac9ea20f3c0501552
Gokul Prathin <gokulprathin8@gmail.com>
2020-04-09 23:14:03 +0530

    Final Card View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+  posts = [
+    { title: 'Neat Tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      username: '@nature',
+      content: 'I saw this tree at hiking today!'
+    },
+    { title: 'Snowy Mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      username: '@mountainLover',
+      content: 'Here is a photo of a snowy mountain'
+    },
+    { title: 'Mountain Biking',
+      imageUrl: 'assets/images/biking.jpeg',
+      username: '@rider',
+      content: 'I did some biking today!'
+    }
+  ]
 }
